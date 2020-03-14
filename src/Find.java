3c1dc1b3297b14aca86b6f52e60c36ace637df73
Black Panther <gamernapster@gmail.com>
2020-03-14 11:48:03 +0100

    one error

--- src/Find.java
+++ src/Find.java
@@ -1,87 +1,38 @@
-import java.util.ArrayList;
+
+// based on hiropons answer: https://stackoverflow.com/questions/60676694/write-my-own-find-and-replace-for-wikipedia-in-java/60679480#60679480
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Find {
 
-	ArrayList<String> list = new ArrayList<>();
-
-	void isObject(String str) {
-
-		String word = "";
-
-		for (int i = 0; i < str.length(); i++) {
-			char o = str.charAt(i);
-			word = "";
-			if (o == ':' && str.charAt(i + 1) == '<') {
-				while (str.charAt(i) != '>') {
-					word += str.charAt(i);
-					i++;
-				}
-			} else if (o == '<') {
-				if (str.charAt(i + 1) != 'm' || str.charAt(i + 1) == 'r') {
-					while (str.charAt(i) == '>') {
-						i++;
-					}
-				} else if (str.charAt(i) == 'r') {
-					int count = 0;
-					while (count != 2) {
-						if (str.charAt(i) == '>') {
-							count++;
-						}
-						i++;
-					}
-				} else {
-					while (str.charAt(i) != '>') {
-						word += str.charAt(i);
-						i++;
-					}
-				}
-			} else if (o == '[' && str.charAt(i + 1) == '[') {
-				while (str.charAt(i) != '|') {
-					i++;
-				}
-				i++;
-				while (str.charAt(i) != ']') {
-					word += str.charAt(i);
-				}
-				i++;
-			} else {
-				while (str.charAt(i) != ' ') {
-					word += str.charAt(i);
-					i++;
-				}
-			}
-
-			list.add(word);
-		}
-	}
-
 	String replaceRules(String str) {
 
-		this.isObject(str);
-		String res = "";
+		String regex1 = "(<math>)(.*?)(</math>)";
+		Pattern p1 = Pattern.compile(regex1);
+		String regex2 = "(:<math>)(.*?)(</math>)";
+		Pattern p2 = Pattern.compile(regex2);
 
-		for (int i = 0; i < this.list.size(); i++) {
-			switch (this.list.get(i)) {
-			case ":<math>":
-				res += "\\begin{equation}" + '\n';
-				while (this.list.get(i) != "</math>") {
-					i++;
-					res += this.list.get(i) + ' ';
-				}
-				res += '\n' + "\\end{equation}" + '\n';
-				break;
-			case "<math>":
-				res += "$";
-				while (this.list.get(i) != "</math>") {
-					i++;
-					res += this.list.get(i) + ' ';
-				}
-				res += "$";
-				break;
-			}
-		}
+		String regex3 = "[[.*|";
+		Pattern p3 = Pattern.compile(regex3);
 
-		return res;
+		String replaced = str.replaceAll("<!--.*-->", "");
+		replaced = str.replaceAll("<ref.*</ref>", "");
+		replaced = str.replaceAll("\\{\\{.*\\}\\}", "");
+
+		Matcher m2 = p2.matcher(replaced);
+		replaced = m2.replaceAll("\\\\\\begin\\{equation}\n$2\n\\\\\\end\\{equation\\}\n");
+
+		Matcher m1 = p1.matcher(replaced);
+		replaced = m1.replaceAll("\\$$2\\$");
+
+		Matcher m3 = p3.matcher(replaced);
+		replaced = m3.replaceAll("");
+
+		replaced = replaced.replaceAll("]]", "");
+		replaced = replaced.replaceAll("[[", "");
+
+		return replaced;
+
 	}
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,22 @@
-import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import java.awt.Color;
-import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class Main extends JFrame {
 
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField inputField;
-	private JTextField outputField;
+	private JTextArea inputField;
+	private JTextArea outputField;
 
 	/**
 	 * Launch the application.
@@ -41,52 +40,41 @@ public class Main extends JFrame {
 	public Main() {
 		setTitle("Wikipedia to LaTeX");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1154, 770);
 		contentPane = new JPanel();
+		contentPane.setMinimumSize(new Dimension(1500, 1500));
+		contentPane.setSize(new Dimension(1500, 1500));
+		contentPane.setPreferredSize(new Dimension(1500, 1500));
 		contentPane.setBackground(Color.ORANGE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 
 		JButton onClick = new JButton("Convert");
+		onClick.setBorder(null);
 		onClick.setForeground(Color.WHITE);
 		onClick.setBackground(Color.DARK_GRAY);
-
-		inputField = new JTextField();
-		inputField.setColumns(10);
-
-		outputField = new JTextField();
-		outputField.setForeground(Color.WHITE);
-		outputField.setBackground(Color.DARK_GRAY);
-		outputField.setEditable(false);
-		outputField.setColumns(10);
 
 		onClick.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				Find sr = new Find();
-				String txt = inputField.getText();
-				outputField.setText(sr.replaceRules(txt));
+				outputField.setText(sr.replaceRules(inputField.getText()));
 			}
 
 		});
+		contentPane.setLayout(new GridLayout(0, 1, 0, 0));
 
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup().addContainerGap()
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-								.addComponent(inputField, Alignment.LEADING).addComponent(outputField,
-										Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 406, Short.MAX_VALUE))
-						.addContainerGap(22, Short.MAX_VALUE))
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-						.addContainerGap(181, Short.MAX_VALUE).addComponent(onClick).addGap(161)));
-		gl_contentPane
-				.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(Alignment.TRAILING,
-						gl_contentPane.createSequentialGroup().addContainerGap()
-								.addComponent(inputField, GroupLayout.PREFERRED_SIZE, 82, GroupLayout.PREFERRED_SIZE)
-								.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addComponent(outputField, GroupLayout.PREFERRED_SIZE, 82, GroupLayout.PREFERRED_SIZE)
-								.addGap(18).addComponent(onClick).addGap(37)));
-		contentPane.setLayout(gl_contentPane);
+		inputField = new JTextArea();
+		inputField.setForeground(Color.WHITE);
+		inputField.setCaretColor(Color.WHITE);
+		inputField.setBackground(Color.DARK_GRAY);
+		inputField.setBorder(null);
+		contentPane.add(inputField);
+
+		outputField = new JTextArea();
+		outputField.setBorder(null);
+		contentPane.add(outputField);
+		contentPane.add(onClick);
 	}
 }
